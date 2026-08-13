76ba57bcfbf682ff7a6e4efaf6865272c1620c91
Grace Bigwood <s4097358@student.rmit.edu.au>
2026-08-13 10:41:11 +1000

    feat: remove about me from team page to match requirements,

--- frontend/src/features/team/components/TeamList.tsx
+++ frontend/src/features/team/components/TeamList.tsx
@@ -16,17 +16,15 @@ export function TeamList() {
     <div className="max-w-2xl space-y-6">
     <ul className="space-y-4 rounded-lg border border-zinc-200 bg-white p-6 shadow-sm dark:border-zinc-800 dark:bg-zinc-900">
       {team.map((member) => (
-        <li key={member.name}>
-          <h3 className="text-xl font-semibold">{member.name}</h3>
-          {/* <img src={member.photoURL || 'default-profile.svg'} alt={member.name} className="h-50 w-50 rounded-full m-4" /> */}
+        <li key={member.uid}>
+          <h3 className="text-lg font-semibold">{member.name}</h3>
           <img src={`/team/${member.name}.jpg`} alt={member.name} className="h-50 w-50 rounded-full m-4" onError={(e) => e.currentTarget.src='team/default-profile.svg'}/>
-          <p className="text-sm font-medium tracking-wide text-zinc-400 uppercase">ROLE</p>
-          <p className="mt-1 text-sm">{member.role}</p>
+          <p className="text-lg font-medium tracking-wide text-zinc-400 uppercase">ROLE</p>
+          <p className="mt-1 text-base ">{member.role}</p>
           <p className="text-sm font-medium tracking-wide text-zinc-400 uppercase">EMAIL</p>
           <p className="mt-1 text-sm">{member.email}</p>
-          <p className="text-sm font-medium tracking-wide text-zinc-400 uppercase">ABOUT </p>
-          <p className="mt-1 text-sm">{member.about}</p>
-          <button className="text-xs tracking-wide text-blue-300 uppercase hover:text-blue-500" >edit about</button>
+          {/* <p className="text-sm font-medium tracking-wide text-zinc-400 uppercase">ABOUT </p>
+          <p className="mt-1 text-sm">{member.about}</p> */}
         </li>
       ))}
     </ul>
